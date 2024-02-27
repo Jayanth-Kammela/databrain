1e401a7b3831135c7395d2795126dc7c40acb102
Jayanth <chintu78299@gmail.com>
2024-02-27 14:07:30 +0530

    addesd

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,95 +1,98 @@
 import React, { useEffect, useState } from 'react'
-import { productsService } from '../services/services'
-import styles from "../app.module.css";
+import { productsService } from '../services/services';
 import Card from '../components/Card';
-
-export interface Product {
-    id: string;
-    title: string;
-    price: number;
-    category: string;
-    image: string;
-    rating: any
-}
+import styles from "../app.module.css";
 
 const Products = () => {
 
-    const [data, setData] = useState<Product[]>([])
-    const [originalData, setOriginalData] = useState<Product[]>([]);
-    const [search, setSearch] = useState<string>('')
-    const [load, setLoad] = useState<boolean>(false)
-    const [sortOrder, setSortOrder] = useState(false)
+    interface Product {
+        id: string;
+        title: string;
+        price: number;
+        category: string;
+        image: string;
+        rating: any
+    }
 
-    const forGetProducts = async () => {
+    const [data, setData] = useState<Product[]>([]);
+    const [originalData, setOriginalData] = useState<Product[]>([]);
+    const [load, setLoad] = useState<boolean>(false);
+    const [search, setSearch] = useState<string>("");
+    const [sort, setSort] = useState<boolean>(false)
+
+    const getProducts = async () => {
         try {
             setLoad(true)
-            const res = await productsService();
-            setData(res);
-            setOriginalData(res);
-        } catch (error) {
-            console.log(error);
+            const response = await productsService()
+            setData(response);
+            setOriginalData(response);
             setLoad(false)
+        } catch (error: any) {
+            setLoad(false)
+            throw Error(error.message)
         }
     }
 
-    const forFilter = (newItem: string) => {
-        if (newItem === '') {
+    const forFilter = (item: string) => {
+        if (item === '') {
             setData(originalData);
         } else {
-            const filterData = originalData.filter((item: Product) => {
-                return item.category.toLocaleLowerCase() === newItem.toLocaleLowerCase();
+            const filteredData = [...originalData].filter((itemx: Product) => {
+                return itemx.category.toLowerCase() === item.toLowerCase();
             });
-            setData(filterData);
+            setData(filteredData);
         }
-    };
+    }
+    
 
-
-    const forSortTitle = () => {
-        setSortOrder(!sortOrder)
-        const res = [...data].sort((a: Product, b: Product) => {
-            return sortOrder ? (a.title > b.title ? 1 : -1) : (a.title < b.title ? 1 : -1)
+    const sortingFunTitle = () => {
+        setSort(!sort)
+        const sortedData = [...data].sort((x: Product, y: Product) => {
+            return (sort ? (x.title < y.title ? 1 : -1) : (x.title > y.title ? 1 : -1))
         })
-        setData(res)
+        setData(sortedData);
     }
 
-    const forSortRating = () => {
-        setSortOrder(!sortOrder)
-        const res = [...data].sort((a: Product, b: Product) => {
-            return sortOrder ? (a.rating.rate > b.rating.rate ? 1 : -1) : (a.rating.rate < b.rating.rate ? 1 : -1)
+    const sortingFunRating = () => {
+        setSort(!sort);
+        const sortedData = [...data].sort((x: Product, y: Product) => {
+            return sort ? (x.rating.rate < y.rating.rate ? 1 : -1) : (x.rating.rate > y.rating.rate ? 1 : -1)
         })
-        setData(res)
-    }
+        console.log(sortedData);
 
+        setData(sortedData)
+    }
 
     useEffect(() => {
-        forGetProducts()
+        getProducts()
     }, [])
+
 
     return (
         <React.Fragment>
 
-
             <div className="flex justify-center my-8">
                 <input className={`${styles.input} py-3 px-4 my-1 block w-4/5 bg-white text-md border-solid border-2 border-black`}
                     type="text"
-                    placeholder="Search products by title & category..."
-                    value={search}
-                    onChange={(e) => setSearch(e.target.value)}
-                />
+                    placeholder="Search products by title & category..." value={search} onChange={(e: any) => setSearch(e.target.value)} />
             </div>
 
-
-            <div className="flex justify-around items-center flex-col md:flex-row sm:flex-row">
+            <div className="flex justify-around items-baseline my-8">
                 <div>
-                    Sort By : <button type="button" onClick={forSortTitle} className="text-gray-900 bg-white border border-gray-300 font-medium rounded-3xl text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600">Title</button>
-
-                    <button type="button" onClick={forSortRating} className="text-gray-900 bg-white border border-gray-300 font-medium rounded-3xl text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600">Rating</button>
+                    Sort by :
+                    <button className="text-gray-900 bg-white border border-gray-300 font-medium rounded-3xl text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600" onClick={sortingFunTitle}>
+                        Title
+                    </button>
+                    <button className="text-gray-900 bg-white border border-gray-300 font-medium rounded-3xl text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600" onClick={sortingFunRating}>
+                        Rating
+                    </button>
                 </div>
 
-
                 <div>
-                    Filter By : <button className="text-gray-900 bg-white border border-gray-300 font-medium rounded-3xl text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600" onClick={() => forFilter('')}>All</button>
-
+                    Filter by :
+                    <button className="text-gray-900 bg-white border border-gray-300 font-medium rounded-3xl text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600" onClick={()=>forFilter('')}>
+                        All
+                    </button>
                     <button className="text-gray-900 bg-white border border-gray-300 font-medium rounded-3xl text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600" onClick={() => forFilter('electronics')}>Electronics</button>
                     <button className="text-gray-900 bg-white border border-gray-300 font-medium rounded-3xl text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600" onClick={() => forFilter("jewelery")}>Jewelery</button>
                     <button className="text-gray-900 bg-white border border-gray-300 font-medium rounded-3xl text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600" onClick={() => forFilter("men's clothing")}>Mens</button>
@@ -97,17 +100,16 @@ const Products = () => {
                 </div>
             </div>
 
-            <div className='flex flex-wrap justify-center items-center h-full'>
-                {load ? data.filter((item: Product) => {
-                    return (
-                        item.title.toLowerCase().includes(search.toLowerCase()) ||
-                        item.category.toLowerCase().includes(search.toLowerCase())
-                    );
-                }).map((item: Product) => {
-                    return <Card key={item.id} item={item} />;
-                }) : 'loading...'}
-            </div>
+            <div className="flex flex-wrap justify-center items-center">
+                {
+                    load ? <h1>Loading...</h1> : data.filter((item: Product) => {
+                        return (item.category.toLowerCase().includes(search.toLowerCase()) || item.title.toLowerCase().includes(search.toLowerCase()))
+                    }).map((item: Product) => {
+                        return <Card key={item.id} item={item} />
+                    })
 
+                }
+            </div>
         </React.Fragment>
     )
 }
